598b210346aa32669edf873be193d0d65a6d51ef
Marc-Antoine <marco38000@gmail.com>
2020-10-29 10:42:08 +0100

    feat : add header in card

--- chat-server-monitoring/src/Components/count-chatrooms.js
+++ chat-server-monitoring/src/Components/count-chatrooms.js
@@ -3,6 +3,7 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import ActiveServers from './active-servers'
+import { CardHeader } from '@material-ui/core';
 
 class CountChatRooms extends React.Component {
     constructor(props) {
@@ -22,10 +23,8 @@ class CountChatRooms extends React.Component {
         return (
             <div>
                 <Card className="count-card">
+                    <CardHeader className="title" title="Chatrooms"></CardHeader>
                     <CardContent>
-                        <Typography className="title">
-                            Chatrooms
-                        </Typography>
                         <Typography variant="h2">
                             {this.state.chatrooms.length}
                         </Typography>
