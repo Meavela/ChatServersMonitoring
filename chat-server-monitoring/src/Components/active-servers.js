ee34c6a615af39be962b98983d3e0b308fcb28d8
Marc-Antoine <marco38000@gmail.com>
2020-10-29 10:32:32 +0100

    Feat : add json to list servers

--- chat-server-monitoring/src/Components/active-servers.js
+++ chat-server-monitoring/src/Components/active-servers.js
@@ -2,8 +2,9 @@ import React from 'react';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import Servers from '../servers.json'
 
-const rows = ['http://127.0.0.1:4567', 'http://127.0.0.1:2435'];
+const rows = Servers.names;
 
 export default function ActiveServers(){
     return (
