06e2911bcd8bd75ed9584711fb16ee13596d42ad
Lou BEGE <wolfla@hotmail.fr>
2020-10-29 11:49:29 +0100

    style : align items
    style : align items

--- chat-server-monitoring/src/Components/home.js
+++ chat-server-monitoring/src/Components/home.js
@@ -3,18 +3,27 @@ import ActiveUsers from './active-users';
 import ActiveChatrooms from './count-chatrooms';
 import ListUsers from './list-users';
 import ListChatrooms from './list-chatrooms';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
 
 class Home extends React.Component {
 
     render() {
         return (
-            <div>
-                <ActiveUsers />
-                <ActiveChatrooms />
+            <Box>
+                <Grid container>
+                    <Grid item xs={6}>
+                        <ActiveUsers flexGrow={1} />
+                    </Grid>
+                    <Grid item xs={6}>
+                        <ActiveChatrooms flexGrow={1}/>
+                    </Grid>
+                </Grid>
                 <br />
                 <ListUsers />
+                <br />
                 <ListChatrooms />
-            </div>
+            </Box>
         )
     }
 }
