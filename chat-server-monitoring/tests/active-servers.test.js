568771753967e8910f6df5cd108cb887b790a334
Lou BEGE <wolfla@hotmail.fr>
2020-11-28 14:37:35 +0100

    Tests: Add test for active-servers

---
+++ chat-server-monitoring/tests/active-servers.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import ActiveServers from '../src/Components/active-servers';
+
+test('Display active servers', () => {
+    const component = renderer.create(
+        <ActiveServers />,
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();  
+});
