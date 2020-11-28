35a7c9aef31a154dbd4ab057f3ff5f551c1b60ad
Lou BEGE <wolfla@hotmail.fr>
2020-11-28 15:25:14 +0100

    Tests: Add mock on servers.json

--- chat-server-monitoring/tests/active-servers.test.js
+++ chat-server-monitoring/tests/active-servers.test.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { render } from '@testing-library/react';
 import ActiveServers from '../src/Components/active-servers';
 
+// Mock the component to take the fake server url
+jest.mock('../src/Components/active-servers');
+
 test('Display active servers', () => {
+    const {container} = render(<ActiveServers />)
+    expect(container.textContent).toMatch('http://127.0.0.1:7419/unittesthttp://127.0.0.1:6592/unitest2')
+});
+
+test('Not change active servers', () => {
     const component = renderer.create(
         <ActiveServers />,
     );
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();  
 });
-
--- chat-server-monitoring/tests/users.test.js
+++ chat-server-monitoring/tests/users.test.js
@@ -10,7 +10,7 @@ jest.mock('../src/Components/list-users');
 // Check that the Users page display the right things
 test('Display Users page', () => {
     const {container} = render(<Users />)
-    expect(container.textContent).toMatch('Active users2On servers :http://127.0.0.1:7419/unittestUsersServerUser IDUsernameStatus151.11.52.681LouACTIVE172.82.64.792Marc-AntoineACTIVE')
+    expect(container.textContent).toMatch('Active users2On servers :http://127.0.0.1:7419/unittesthttp://127.0.0.1:6592/unitest2UsersServerUser IDUsernameStatus151.11.52.681LouACTIVE172.82.64.792Marc-AntoineACTIVE')
 });
 
 // Check that the Users page don't change
